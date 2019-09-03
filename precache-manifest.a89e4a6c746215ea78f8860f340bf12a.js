ab865ab0a4534363ceb1b189ff832378ca8e4425
nikoskef <nikoskef86@gmail.com>
2019-09-03 10:25:08 +0300

    Updates

---
+++ precache-manifest.a89e4a6c746215ea78f8860f340bf12a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6734aaba5a7aff55321b73c1a89d9f1",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b492ba96c25ea101a991",
+    "url": "/monsters/static/css/main.4e579c98.chunk.css"
+  },
+  {
+    "revision": "e2a6810e5f8b4194b66d",
+    "url": "/monsters/static/js/2.5f1e0dc3.chunk.js"
+  },
+  {
+    "revision": "b492ba96c25ea101a991",
+    "url": "/monsters/static/js/main.30f57dd8.chunk.js"
+  },
+  {
+    "revision": "98e095b3e406d4e12025",
+    "url": "/monsters/static/js/runtime~main.e06e9654.js"
+  }
+]);
